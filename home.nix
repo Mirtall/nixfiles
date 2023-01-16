159c789254db55c9aa6510870c74e42e049372bd
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2023-01-16 21:44:04 +0100

    feat(bash zsh): switched terminal from bash to zsh

--- home.nix
+++ home.nix
@@ -1,25 +1,27 @@
 { config, pkgs, ... }: {
-    imports = [
-        ./packages.nix
-        ./git.nix
-        ./vim.nix
-        ./bash.nix
-        ./urxvt.nix
-        ./bat.nix
-        ./java.nix
-        ./firefox.nix
-        ./xresources.nix
-    ];
+  imports = [
+    ./packages.nix
+    ./git.nix
+    ./vim.nix
+    #./bash.nix
+    ./urxvt.nix
+    ./bat.nix
+    ./java.nix
+    ./firefox.nix
+    ./xresources.nix
+    ./zsh.nix
+  ];
 
-    programs.home-manager.enable = true;
-    home.username = "ethan";
-    home.homeDirectory = "/home/ethan";
+  programs.home-manager.enable = true;
+  home.username = "ethan";
+  home.homeDirectory = "/home/ethan";
+  # users.users.ethan.shell = "zsh";
 
-    # FIXME: own files
-    services.lorri.enable = true;
-    services.dunst.enable = true;
+  # FIXME: own files
+  services.lorri.enable = true;
+  services.dunst.enable = true;
 
-    nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-    home.stateVersion = "22.05";
+  home.stateVersion = "22.05";
 }
--- zsh.nix
+++ zsh.nix
@@ -1,14 +1,18 @@
-{ pkgs, ... }: {
-  programs.bash = {
+{ pkgs, ... }:
+{
+  programs.zsh = {
     enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
 
     shellAliases = {
       cat = "bat";
       remi = "nmcli connection up Remi";
       dtc = "docker run -v ~/dev/epita/tiger/tc:/tc/ --rm -it --user 1000 tiger:stable";
+      toto = "mkdir toto && cd toto && linit $1";
     };
 
-    bashrcExtra = ''
+    initExtra = ''
       function linit {
         if [ $# -eq 0 ]; then
           lorri init && direnv allow
@@ -17,10 +21,15 @@
         fi
       }
 
-      eval "$(direnv hook bash)"
+      eval "$(direnv hook zsh)"
 
       export PGDATA="$HOME/postgres_data"
       export PGHOST="/tmp"
     '';
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "kafeitu";
+    };
   };
 }
