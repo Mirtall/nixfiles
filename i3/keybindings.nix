1e6ccf786acb9f90b7238146f9005f013bc758a2
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2023-05-20 01:16:19 +0200

    i3: added azert shortcut

--- i3/keybindings.nix
+++ i3/keybindings.nix
@@ -4,6 +4,7 @@ let
 in
 lib.mkOptionDefault {
   "${modifier}+Shift+a" = "kill";
+  "${modifier}+Shift+z" = "layout tabbed";
   # Rofi
   "${modifier}+d" = "exec --no-startup-id rofi -show drun";
   "${modifier}+Shift+f " = "exec --no-startup-id rofi -show window";
