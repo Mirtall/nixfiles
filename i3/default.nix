ed397b51938c9aeb61b345a78b611cbccd172fe0
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2023-05-20 12:40:00 +0200

    feat(i3): switch to i3-gaps

--- i3/default.nix
+++ i3/default.nix
@@ -5,11 +5,22 @@ in
 {
   xsession.windowManager.i3 = {
     enable = true;
-    package = pkgs.i3;
+    package = pkgs.i3-gaps;
 
     config = {
       modifier = "Mod4"; # Window key
+      defaultWorkspace = "1";
       keybindings = import ./keybindings.nix { inherit lib; };
+
+      window = {
+        border = 3;
+      };
+
+      gaps = {
+        smartBorders = "on";
+        inner = 5;
+        outer = 0;
+      };
     };
   };
 
